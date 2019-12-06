f9b07d107f1e94068f9d79ef7376923cbda07cbb
Roman Blondel <rblondel23@gmail.com>
2019-12-06 19:46:29 +0100

    make group creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,15 +1,32 @@
 class GroupsController < ApplicationController
+	before_action :authenticate_user!, only: [:new]
+
 	def index
-	@group = Group.all
+	@all_group = []
+	Group.all.each do |group|
+		@all_group << group
+	end 
+
 	end
 
 	def show
+		@group = Group.find(params[:id])
 	end
 
-    def new
+	def new
+		@group = Group.new
 	end
 
 	def create
+    @group = Group.new(admin: current_user, name: params[:group_name], body: params[:group_body]) 
+    if @group.save # essaie de sauvegarder en base @group
+      redirect_to group_path(@group.id)
+    else
+			render 'groups/new', to: 'groups#new'
+			puts "#" * 60
+			puts @group.errors.messages
+			puts "#" * 60
+    end 
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home_page#index'
+  resources :groups do 
+  resources :join_groups end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :follows
