54e90c7e2bd25af9fd3d54cef045e7ac78ab17b1
assouev <bsngr.asv@gmail.com>
2019-12-11 05:32:00 +0100

    filter groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,11 +2,29 @@ class GroupsController < ApplicationController
 	before_action :authenticate_user!, only: [:new]
 
 	def index
-	@all_group = []
+	@all_groups= []
+	@rap = []
+	@rock = []
+	@pop = []
+	@rnb = []
+	@electro = []
 	Group.all.each do |group|
-		@all_group << group
+		@all_groups << group
 	end 
-
+	Group.all.each do |group|
+		if group.group_spe == "Rap/Trap/Drill/Alternatif/Hip-Hop"
+			@rap << group
+		elsif group.group_spe == "Rock/Alternatif/Pop/Metal"
+			@rock << group
+	    elsif group.group_spe == "Pop"
+			@pop << group
+		elsif group.group_spe == "RnB"
+			@rnb << group
+		elsif group.group_spe == "Electro"
+			@electro << group
+				
+		end			
+ end
 	end
 
 	def show
