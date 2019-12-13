68e6b103f55ce22860e0319044f2edecf199ff75
assouev <bsngr.asv@gmail.com>
2019-12-13 14:44:07 +0100

    trying to debug for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'join_groups/create'
-  get 'join_groups/index'
-  get 'join_groups/new'
-  get 'join_groups/edit'
-  get 'join_groups/show'
-  get 'join_groups/update'
-  get 'join_groups/destroy'
   root 'home_page#index'
     devise_for :users, controllers: { registrations: "registrations" }
 
