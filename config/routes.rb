b2dd3d73da511b9282e826c012f12f75f9d4b952
Roman Blondel <rblondel23@gmail.com>
2019-12-15 05:58:37 +0100

    add button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :groups do 
   resources :join_groups end
+  get 'join_groups/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :follows
   resources :groups
