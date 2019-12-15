44a820c4c3d5a491c1267e2abcab792f30aa6ca6
assouev <bsngr.asv@gmail.com>
2019-12-16 00:29:50 +0100

    try to push heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   root 'home_page#index'
     devise_for :users, controllers: { registrations: "registrations" }
 
-  resources :groups
+  resources :groups do 
+  resources :join_groups end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :follows
   resources :groups
-  resources :join_groups
   resources :specialitys
   resources :users
   resources :after_signups
